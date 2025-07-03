fbcd70608674e83ac8fa95a2dc19c6b33af83c5e
José Tejada <jose.anibal0211@gmail.com>
2025-07-03 12:18:26 -0400

    feat(LoanRequestController): populate index method with loan request dummy data

--- app/Http/Controllers/procesos/LoanRequestController.php
+++ app/Http/Controllers/procesos/LoanRequestController.php
@@ -13,6 +13,53 @@ class LoanRequestController extends Controller
 
     public function index(Request $request)
     {
-        return Inertia::render('procesos/solicitudesPrestamos', []);
+        $tiposCuenta = [
+            ['id_tipo_cuenta'=>1, 'tipo_cuenta'=>'PRESTAMOS'],
+            ['id_tipo_cuenta'=>2, 'tipo_cuenta'=>'PRESTAMOS EN DOLAR'],
+        ];
+        $tiposCredito = [
+            ['id_tipo_credito'=>1, 'tipo_credito'=>'CONSUMO'],
+            ['id_tipo_credito'=>2, 'tipo_credito'=>'COMERCIAL'],
+        ];
+        $tiposCalculo = [
+            ['id_tipo_calculo'=>1, 'tipo_calculo'=>'INSOLUTO'],
+            ['id_tipo_calculo'=>2, 'tipo_calculo'=>'SOLUTO'],
+        ];
+        $garantias = [
+            ['id_garantia'=>1, 'garantia'=>'HIPOTECARIA'],
+            ['id_garantia'=>2, 'garantia'=>'VEHICULOS'],
+        ];
+        $formasPago = [
+            ['id_forma_pago'=>1, 'forma_pago'=>'MENSUAL'],
+            ['id_forma_pago'=>2, 'forma_pago'=>'QUINCENAL'],
+        ];
+        $mediosPago = [
+            ['id_medio_pago'=>1, 'medio_pago'=>'TRANSFERENCIA'],
+            ['id_medio_pago'=>2, 'medio_pago'=>'CHEQUE'],
+        ];
+        $oficiales = [
+            ['id_oficial'=>1, 'oficial'=>'MARTIN DE LA CRUZ'],
+            ['id_oficial'=>2, 'oficial'=>'HIPOLITO DE LA CRUZ'],
+        ];
+        $cobradores = [
+            ['id_cobrador'=>1, 'cobrador'=>'OFICINA'],
+            ['id_cobrador'=>2, 'cobrador'=>'LUIS ORTEGA'],
+        ];
+        $necesidades = [
+            ['id_necesidad'=>1, 'necesidad'=>'PARA COMPRAR SOLAR'],
+            ['id_necesidad'=>2, 'necesidad'=>'PARA INVERTIR EN EL NEGOCIO'],
+        ];
+
+        return Inertia::render('procesos/solicitudesPrestamos', [
+            'tipos_cuenta' => $tiposCuenta,
+            'tipos_credito' => $tiposCredito,
+            'tipos_calculo' => $tiposCalculo,
+            'garantias' => $garantias,
+            'formas_pago' => $formasPago,
+            'medios_pago' => $mediosPago,
+            'oficiales' => $oficiales,
+            'cobradores' => $cobradores,
+            'necesidades' => $necesidades,
+        ]);
     }
 }
